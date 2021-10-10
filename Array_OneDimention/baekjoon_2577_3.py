483ba818ce2b26fb0a6c39ba334abec5c5bcbdd4
moner050 <83222976+moner050@users.noreply.github.com>
2021-10-10 23:56:42 +0900

    baekjoon 1차원 배열(5단계) 문제 풀기.

--- Array_OneDimention/baekjoon_2577_3.py
+++ Array_OneDimention/baekjoon_2577_3.py
@@ -10,3 +10,14 @@
     마찬가지로 둘째 줄부터 열 번째 줄까지 A × B × C의 결과에 1부터 9까지의 숫자가 각각 몇 번 쓰였는지 차례로 한 줄에 하나씩 출력한다.
 """
 
+A = int(input())
+B = int(input())
+C = int(input())
+
+if (A < 1000 and A >= 100) and (B < 1000 and B >= 100) and (C < 1000 and C >= 100):
+    result = list(str(A * B * C))
+
+    for i in range(10):
+        print(result.count(str(i)))
+else :
+    print("A,B,C의 값이 100이상 1000미만인 값이 아닙니다.")
