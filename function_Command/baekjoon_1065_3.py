729d2309ecb3d5c49689ccb51d1b5214889d99e2
moner050 <83222976+moner050@users.noreply.github.com>
2021-11-21 23:22:16 +0900

    baekjoon function(함수)(6단계) 문제 풀기

--- function_Command/baekjoon_1065_3.py
+++ function_Command/baekjoon_1065_3.py
@@ -8,15 +8,14 @@
     첫째 줄에 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력한다.
 """
 
-num = int(input())
+N = int(input())
 hansu = 0
 
-for n in range(1, num + 1):
-    if n <= 99:                                     # 1부터 99까지는 모두 한수
+for i in range(1, N + 1):
+    if i <= 99:
         hansu += 1
-
     else:
-        nums = list(map(int, str(n)))               # 숫자를 자릿수대로 분리
-        if nums[0] - nums[1] == nums[1] - nums[2]:  # 등차수열 확인
+        Num = list(map(int, str(i)))
+        if Num[0] - Num[1] == Num[1] - Num[2]:
             hansu += 1
 print(hansu)
